66cc5f1c88b72fff91a118d6565ba7e5e5e1d0a7
gyugenthira_commits <gyugenthira@gmail.com>
2026-03-23 14:57:19 +0530

    UC2: Basic Room Types & Static Availability

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,17 +3,70 @@ package main;
 /**
  * Book My Stay App
  *
- * This class represents the entry point of the Hotel Booking Management System.
- * It demonstrates how a Java application starts execution and prints output.
+ * Use Case 2: Basic Room Types & Static Availability
+ *
+ * Demonstrates abstraction, inheritance, and static availability.
  *
  * @author Saksham
- * @version 1.0
+ * @version 2.0
  */
 
-public class UseCase1HotelBookingApp {
+abstract class Room {
+    String type;
+    int beds;
+    double price;
 
+    Room(String type, int beds, double price) {
+        this.type = type;
+        this.beds = beds;
+        this.price = price;
+    }
+
+    void display() {
+        System.out.println("Room Type: " + type);
+        System.out.println("Beds: " + beds);
+        System.out.println("Price: " + price);
+    }
+}
+
+class SingleRoom extends Room {
+    SingleRoom() {
+        super("Single Room", 1, 1000);
+    }
+}
+
+class DoubleRoom extends Room {
+    DoubleRoom() {
+        super("Double Room", 2, 2000);
+    }
+}
+
+class SuiteRoom extends Room {
+    SuiteRoom() {
+        super("Suite Room", 3, 5000);
+    }
+}
+
+public class UseCase2RoomInitialization {
     public static void main(String[] args) {
-        System.out.println("Welcome to Book My Stay App");
-        System.out.println("Hotel Booking System v1.0");
+
+        Room r1 = new SingleRoom();
+        Room r2 = new DoubleRoom();
+        Room r3 = new SuiteRoom();
+
+        int singleAvailable = 5;
+        int doubleAvailable = 3;
+        int suiteAvailable = 2;
+
+        r1.display();
+        System.out.println("Available: " + singleAvailable);
+        System.out.println();
+
+        r2.display();
+        System.out.println("Available: " + doubleAvailable);
+        System.out.println();
+
+        r3.display();
+        System.out.println("Available: " + suiteAvailable);
     }
 }
