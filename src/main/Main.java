32f2d6f10d87b41b121383022951c1da1323a3fa
gyugenthira_commits <gyugenthira@gmail.com>
2026-03-23 15:01:15 +0530

    UC3: Centralized Room Inventory Management

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,72 +1,54 @@
 package main;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Book My Stay App
  *
- * Use Case 2: Basic Room Types & Static Availability
+ * Use Case 3: Centralized Room Inventory Management
  *
- * Demonstrates abstraction, inheritance, and static availability.
+ * Demonstrates use of HashMap for centralized inventory.
  *
  * @author Saksham
- * @version 2.0
+ * @version 3.0
  */
 
-abstract class Room {
-    String type;
-    int beds;
-    double price;
+class RoomInventory {
+    private Map<String, Integer> inventory;
 
-    Room(String type, int beds, double price) {
-        this.type = type;
-        this.beds = beds;
-        this.price = price;
+    public RoomInventory() {
+        inventory = new HashMap<>();
+        inventory.put("Single Room", 5);
+        inventory.put("Double Room", 3);
+        inventory.put("Suite Room", 2);
     }
 
-    void display() {
-        System.out.println("Room Type: " + type);
-        System.out.println("Beds: " + beds);
-        System.out.println("Price: " + price);
+    public int getAvailability(String roomType) {
+        return inventory.getOrDefault(roomType, 0);
     }
-}
 
-class SingleRoom extends Room {
-    SingleRoom() {
-        super("Single Room", 1, 1000);
+    public void updateAvailability(String roomType, int count) {
+        inventory.put(roomType, count);
     }
-}
 
-class DoubleRoom extends Room {
-    DoubleRoom() {
-        super("Double Room", 2, 2000);
+    public void displayInventory() {
+        for (String type : inventory.keySet()) {
+            System.out.println(type + " Available: " + inventory.get(type));
+        }
     }
 }
 
-class SuiteRoom extends Room {
-    SuiteRoom() {
-        super("Suite Room", 3, 5000);
-    }
-}
-
-public class UseCase2RoomInitialization {
+public class UseCase3InventorySetup {
     public static void main(String[] args) {
 
-        Room r1 = new SingleRoom();
-        Room r2 = new DoubleRoom();
-        Room r3 = new SuiteRoom();
+        RoomInventory inventory = new RoomInventory();
 
-        int singleAvailable = 5;
-        int doubleAvailable = 3;
-        int suiteAvailable = 2;
+        inventory.displayInventory();
 
-        r1.display();
-        System.out.println("Available: " + singleAvailable);
-        System.out.println();
+        System.out.println("\nUpdating Single Room count...\n");
+        inventory.updateAvailability("Single Room", 4);
 
-        r2.display();
-        System.out.println("Available: " + doubleAvailable);
-        System.out.println();
-
-        r3.display();
-        System.out.println("Available: " + suiteAvailable);
+        inventory.displayInventory();
     }
 }
