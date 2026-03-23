3191f73cd0cc0bb161a59fd499917837df2e22de
gyugenthira_commits <gyugenthira@gmail.com>
2026-03-23 14:46:33 +0530

    Base setup

---
+++ src/main/Main.java
@@ -0,0 +1,20 @@
+package main;
+
+/*
+ * Hotel Booking Management System
+ *
+ * Objective:
+ * Demonstrates use of Core Java and Data Structures in a real-world booking system.
+ *
+ * Key Concepts:
+ * FIFO handling, room availability, prevention of double booking, OOP design.
+ *
+ * Outcome:
+ * Understand practical usage of data structures in system design.
+ */
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
